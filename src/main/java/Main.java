cb00532a7ab851a45c149f216ca5217e4cf68484
Anna Savrova <annsavrova@gmail.com>
2023-07-02 20:24:45 +0700

    added class TelegramBot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
 
-    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=hmfXdzTeW4270S6wK7mfKCkH6JdOAZn5mIhDYD4h";
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws TelegramApiException {
+        new TelegramBot();
     }
+
 }
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -5,7 +5,6 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-
 import java.io.IOException;
 
 public class Utils {
